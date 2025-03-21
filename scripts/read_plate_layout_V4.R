482d17e44991c2f26290c6ebc9edf8a3d4e57553
Mongelard Fabien <fabien.mongelard@gmail.com>
2025-03-21 11:20:45 +0100

    new pseudo branch for tests

--- scripts/read_plate_layout_V4.R
+++ scripts/read_plate_layout_V4.R
@@ -2,7 +2,7 @@ library(xml2)
 library(tidyverse)
 library(here)
 
-# Load the XML file
+# Load the revvity XML file
 xml_file <- "2577f4a7-3b8f-4c5c-aece-cda68e3d69c7.xml"  # Change this to the correct path
 xml_data <- read_xml(here("data", xml_file))
 
